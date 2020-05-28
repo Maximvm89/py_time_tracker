0203904dd77327124e651f9dd9a558a6698f6d23
Marco Parisi <marco.parisi89@gmail.com>
2020-05-28 08:07:12 +0100

    Refining some UX

--- time_tracker.py
+++ time_tracker.py
@@ -38,8 +38,9 @@ class TimeTracker:
     def start_tracking(self, task_name="Default"):
         # let's check if a json file already exists
         self.data_out_json = os.path.join(document_path, task_name + '.json')
-        print "[+] Looking for {}".format(self.data_out_json)
         if os.path.isfile(self.data_out_json):
+            print "[+] Found an existing {}".format(self.data_out_json)
+            print "[+] Loading previous time track data"
             # If the file exists we want to load the dictionary data instead of starting from scratch
             try:
                 with open(self.data_out_json) as f:
@@ -47,6 +48,8 @@ class TimeTracker:
             except ValueError:
                 print "[-] Problem reading {}. Storing json into {}_temp".format(self.data_out_json, task_name)
                 self.data_out_json = os.path.join(document_path, task_name + '_temp.json')
+        else:
+            print "[+] Creating a new time track data instance"
 
         while self.i <= 1:
             time.sleep(3)
@@ -81,50 +84,51 @@ class TimeTracker:
             json.dump(self.windows, out_file, indent=1)
 
     def stop_tracking(self):
-        print "[+] Received keyboard interrupt"
+        print "[-] Received keyboard interrupt"
+        print "[-] Time tracker interrupted"
         self.i = 2  # should interrupt the tracking
         self.__dump_data()
-        # Show the task available
-        # Create a new task
-        print "[-] Time tracker interrupted"
-        print "[+] Do you want to continue an existing task? [y][n]"
         y = "y"
         n = "n"
+        # Show the task available
+        # Create a new task
+        print "[+] Do you want to start track a new task? [y][n]"
         user_input = input()
         if user_input.lower() == "y":
-            print "[-] Task available"
-            # Create a dictionary of possible task to switch in
-            available_tasks = {}
-            for dirpath, dirnames, filenames in os.walk(self.document_path):
-                count = 0
-                for json_file in filenames:
-                    if json_file.endswith(".json"):
-                        print "\t[{}] {}".format(count, json_file.replace(".json", ""))
-                        available_tasks[count] = json_file.replace(".json", "")
-                        count += 1
-            print "[-] Select the number of the task you want to switch back"
+            print "[+] Insert the name of the new task to track:"
             user_input = raw_input()
+            self.i = 0
+            self.windows = {}
             try:
-                self.i = 0
-                self.windows = {}
-                try:
-                    self.start_tracking(available_tasks[int(user_input)])
-                except KeyboardInterrupt:
-                    self.stop_tracking()
-            except KeyError:
-                print "[-] Error: couldnt find any task with the input:{}".format(user_input)
+                self.start_tracking(user_input)
+            except KeyboardInterrupt:
+                self.stop_tracking()
+
         else:
-            print "[+] Do you want to start track a new task? [y][n]"
+            print "[+] Do you want to continue an existing task? [y][n]"
             user_input = input()
             if user_input.lower() == "y":
-                print "[+] Insert the name of the new task to track:"
+                print "[-] Task available"
+                # Create a dictionary of possible task to switch in
+                available_tasks = {}
+                for dirpath, dirnames, filenames in os.walk(self.document_path):
+                    count = 0
+                    for json_file in filenames:
+                        if json_file.endswith(".json"):
+                            print "\t[{}] {}".format(count, json_file.replace(".json", ""))
+                            available_tasks[count] = json_file.replace(".json", "")
+                            count += 1
+                print "[-] Select the number of the task you want to switch back"
                 user_input = raw_input()
-                self.i = 0
-                self.windows = {}
                 try:
-                    self.start_tracking(user_input)
-                except KeyboardInterrupt:
-                    self.stop_tracking()
+                    self.i = 0
+                    self.windows = {}
+                    try:
+                        self.start_tracking(available_tasks[int(user_input)])
+                    except KeyboardInterrupt:
+                        self.stop_tracking()
+                except KeyError:
+                    print "[-] Error: couldnt find any task with the input:{}".format(user_input)
             else:
                 print "[+] Closing time tracker"
                 exit()
